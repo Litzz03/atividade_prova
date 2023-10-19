7fc8e61918a202db20699a2fec4dffb3b2ccef20
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2023-10-19 20:47:50 -0300

    Correções

--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -21,31 +21,36 @@ export function Home (){
         setAcrescimoAno(0.0)
         setTotal(1000.00)
     }
-    function calcular(){
-        const totalCalculo = 1000
-        if (Idade <= 22){
-            totalCalculo = totalCalculo + (totalCalculo* 0.20)
-            setAcrescimoIdade(totalCalculo*0.20)
-        } else if (idade <= 28){
-            totalCalculo = totalCalculo + (totalCalculo * 0.18)
-            setAcrescimoIdade(totalCalculo*0.18)
-        }else {
-            totalCalculo = totalCalculo - (totalCalculo* 0.15)
-            setAcrescimoIdade(totalCalculo*0.15)
-        }
 
-        if (ano <= 2000){
-            totalCalculo = totalCalculo + (totalCalculo* 0.30)
-            setAcrescimoAno(totalCalculo*0.30)
-        } else if (ano <= 2009){
-            totalCalculo = totalCalculo + (totalCalculo* 0.15)
-            setAcrescimoAno(totalCalculo*0.15)
-        } else if (ano <= 2015){
+    function calcular() {
+        const totalCalculo = 1000;
+        let novoTotal = totalCalculo;
+    
+        if (idade <= 22) {
+            novoTotal += novoTotal * 0.20;
+            setAcrescimoIdade(totalCalculo * 0.20);
+        } else if (idade <= 28) {
+            novoTotal += novoTotal * 0.18;
+            setAcrescimoIdade(totalCalculo * 0.18);
         } else {
-            totalCalculo = totalCalculo - (totalCalculo* 0.10)
-            setAcrescimoAno(totalCalculo*0.10)
+            novoTotal -= novoTotal * 0.15;
+            setAcrescimoIdade(totalCalculo * 0.15);
         }
-        setTotal(totalCalculo)
+    
+        if (ano <= 2000) {
+            novoTotal += novoTotal * 0.30;
+            setAcrescimoAno(totalCalculo * 0.30);
+        } else if (ano <= 2009) {
+            novoTotal += novoTotal * 0.15;
+            setAcrescimoAno(totalCalculo * 0.15);
+        } else if (ano <= 2015) {
+            setAcrescimoAno(0.0)
+        } else {
+            novoTotal -= novoTotal * 0.10;
+            setAcrescimoAno(totalCalculo * 0.10);
+        }
+    
+        setTotal(novoTotal);
     }
     return(
         <View style={styles.container}>
@@ -53,7 +58,7 @@ export function Home (){
                 <Text style={styles.titulo}>SIMULACAR</Text>
             </View>
             <InputGenerico titulo="Nome" width={styles.inputNome}/>
-            <InputGenerico titurlo="Carro" width={styles.inputCarro}/>
+            <InputGenerico titulo="Carro" width={styles.inputCarro}/>
             <View style={styles.conjuntoInputs}>
                 <InputGenerico titulo="Idade" width={styles.inputIdade} value={idade} setValor={setIdade}/>
                 <InputGenerico titulo="Ano" width={styles.inputAno} value={ano} setValor={setAno}/>
